efcb4357598d46d848d1280d7b41e2c29f4ac573
Daniel Nuno <danunora@smtp.localdomain>
2017-01-06 16:24:38 -0600

    producto

--- Vending/README.txt
+++ Vending/README.txt
@@ -1,7 +1,20 @@
-Base de datos para el control del Vending
+#############################################
+# Base de datos para el control del Vending #
+#############################################
 
-Maquinas,
+############
+# Maquinas #
+############
 Tabla para el control de las maquinas, tiene los siguientes campos:
-- Numero de Serie
-- Modelo
-- Ubicacion
+- Numero de Serie, debe ser unico y llave primaria
+- Modelo, no puede estar vacio
+- Ubicacion (Cotton, Fedex, Comude, Progreso, Toshiba)
+
+#############
+# Productos #
+#############
+Tabla para el control de los productos, tiene los siguientes campos:
+- Producto ID, debe ser unico y llave primaria
+- Nombre del producto, no puede estar vacio
+- Categoria (Fritura, Bebida, Botana, Chocolate, Barras, Dulces, Galletas)
+- Descripcion
--- Vending/maquinas.py
+++ Vending/maquinas.py
@@ -3,34 +3,69 @@
 import sqlite3
 
 def insert(db, row):
-    db.execute('INSERT INTO maquinas (maqsn, maqmod, maqloc) VALUES (?, ?, ?)', (row['maqsn'], row['maqmod'], row['maqloc']))
+    db.execute('INSERT INTO maquinas (maqid, maqmod, maqloc) VALUES (?, ?, ?)', (row['maqid'], row['maqmod'], row['maqloc']))
     db.commit()
 
-def retrieve(db, maqsn):
-    cursor = db.execute('SELECT * FROM maquinas WHERE maqsn = ?', (maqsn,))
+def retrieve(db, maqid):
+    cursor = db.execute('SELECT * FROM maquinas WHERE maqid = ?', (maqid,))
     return cursor.fetchone()
 
-def disp_rows(db):
-    cursor = db.execute('SELECT * FROM maquinas ORDER BY maqsn')
+def disp_rows(db, tableid):
+    cursor = db.execute('SELECT * FROM maquinas ORDER BY maqid')
     for row in cursor:
-        print('  {}: {}: {}'.format(row['maqsn'], row['maqmod'], row['maqloc']))
+        print('  {}: {}: {}'.format(row['maqid'], row['maqmod'], row['maqloc']))
 
 ##### Main #####
 def main():
+
+#def create_db():
     db = sqlite3.connect('maquinas.db')
     db.row_factory = sqlite3.Row
-    print('Create table maquinas')
+    print('Creating tables...')
+    print('CREATE TABLE maquinas')
     db.execute('DROP TABLE IF EXISTS maquinas')
-    db.execute('CREATE TABLE maquinas ( maqsn text, maqmod text, maqloc text )')
+    db.execute('CREATE TABLE maquinas ( maqid text PRIMARY KEY NOT NULL, maqmod text NOT NULL, maqloc text NOT NULL)')
+    print('CREATE TABLE producto')
+    db.execute('DROP TABLE IF EXISTS producto')
+    db.execute('CREATE TABLE producto ( prodid int PRIMARY KEY NOT NULL, prodname text NOT NULL, prodcate text NOT NULL, proddesc text)')
+
+##### Main #####
+#def main():
+#    create_db()
 
 # Ask for the values
+#   maqidx = input("Numero Serial? ")
+#   maqmodx = input("Modelo? ")
+#   maqlocx = input("Localidad? ")
+#   insert(db, dict(maqid = maqidx, maqmod = maqmodx, maqloc = maqlocx))
 
-    maqsnx = input("Numero Serial? ")
-    maqmodx = input("Modelo? ")
-    maqlocx = input("Localidad? ")
+# Test of inserting rows
+# maquinas ( maqid text PRIMARY KEY NOT NULL, maqmod text NOT NULL, maqloc text NOT NULL)')
+    print('Inserting rows for maquinas...')
+    insert(db, dict(maqid = '1234a', maqmod = 'AMS-360' , maqloc = 'PROGRESO'))
+    insert(db, dict(maqid = '1234A', maqmod = 'AMS-360' , maqloc = 'FEDEX'))
+#
+# Duplicates are not allowed, but A != a
+#    insert(db, dict(maqid = '1234A', maqmod = 'AMS-360' , maqloc = 'COMUDE'))
+#
+    insert(db, dict(maqid = '1234b', maqmod = 'AMS-360' , maqloc = 'TOSHIBA'))
+    insert(db, dict(maqid = '1234c', maqmod = 'AMS-360' , maqloc = 'COTTON'))
+    print('Inserting successfully')
 
-    print('Create rows')
-    insert(db, dict(maqsn = maqsnx, maqmod = maqmodx, maqloc = maqlocx))
+# Test of inserting rows
+# producto ( prodid int PRIMARY KEY NOT NULL, prodname text NOT NULL, prodcate text NOT NULL, proddesc text)')
+    print('Inserting rows for producto...')
+    insert(db, dict(prodid = '1234a', prodname = 'AMS-360' , prodcate = 'PROGRESO', proddesc = ''))
+    insert(db, dict(prodid = '1234b', prodname = 'AMS-360' , prodcate = 'PROGRESO', proddesc = ''))
+#
+# Duplicates are not allowed, but A != a
+    insert(db, dict(prodid = '1234a', prodname = 'AMS-360' , prodcate = 'PROGRESO', proddesc = ''))
+#
+    insert(db, dict(prodid = '1234c', prodname = 'AMS-360' , prodcate = 'PROGRESO', proddesc = ''))
+    insert(db, dict(prodid = '1234d', prodname = 'AMS-360' , prodcate = 'PROGRESO', proddesc = ''))
+    print('Inserting successfully')
+
+# Retriving rows
     disp_rows(db)
 	
 if __name__ == "__main__": main()
