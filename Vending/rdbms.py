d22dd277ce38029fa72c5cf952e092f62cd62f54
Daniel Nuno <daniel.nuno@gmail.com>
2017-01-14 18:05:02 -0600

    rdbms

---
+++ Vending/rdbms.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# daniel.nuno@gmail.com
+
+# 
+class database:
+    def __init__(self, **kwargs):
+        self.filename = kwargs.get('filename')
+        self.table = kwargs.get('table', 'test')
+
+def main():
+
+if __name__ == "__main__": main()
